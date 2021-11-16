062125bb4fe59b1581f0d1249e5831a372bd1473
Isaac <31235653+alfred5063@users.noreply.github.com>
2021-11-16 08:35:10 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+from keras.models import load_model
+
+app = Flask(__name__, template_folder = 'template')
+model = load_model('\\model\\model.h5')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(int_features)
+    
+    output = round(prediction[0], 2)
+    
+    return render_template('index.html',
+                           prediction_test = 'Prediction Percentage $ {}'.format(output))
+
+if __name__ == "__main__":
+    app.run(debug = True)
